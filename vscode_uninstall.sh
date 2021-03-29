88acc50c123fcddc796979fdad5add4bbb37603c
rohitgeddam <48797475+rohitgeddam@users.noreply.github.com>
2021-03-29 16:57:12 +0530

    Create vscode_uninstall.sh

---
+++ vscode_uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rfv "$HOME/.vscode"
+rm -rfv "$HOME/Library/Application Support/Code"
+rm -rfv "$HOME/Library/Caches/com.microsoft.VSCode"
+rm -rfv "$HOME/Library/Saved Application State/com.microsoft.VSCode.savedState"
